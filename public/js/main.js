5afdf056e0961e2d3bf563f3de9312b11d77e526
Chanx Singha <chanx.designs@gmail.com>
2016-02-28 19:54:47 +0530

    Just A simple Update

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,36 @@
 /**
 
  **/
+var window = window || {};
 
-(function (global) {
-    //if ()
+var RESUME = (function (window) {
+    var elem,
+        regex;
+
+    var MOD = {};
+
+    MOD.prototype = {
+        constructor: MOD,
+        //Verify name function
+        verifyName: function () {
+            var value;
+            regex = /[^0-9][a-zA-Z]\w*/g;
+            elem = document.querySelector("#name");
+            value = elem.value;
+            if (regex.test(value)) {
+                elem.style.border = "1px solid green";
+            } else {
+                elem.style.border = "1px solid red";
+            }
+        }
+    };
+
+    function email() {
+        regex = /a-zA-Z/
+    }
+
+    return MOD;
+
 })(window);
+
+console.log(window);
